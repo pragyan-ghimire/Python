c4f74dc94fd383936de8111f1cf1f6bb5e2e6421
pragyan <ghimirepragyan100@gmail.com>
2025-06-02 12:14:20 +0545

    string methods

---
+++ Day1/stringMethods.py
@@ -0,0 +1,12 @@
+name = "pragyan ghimire"
+# name = "12"
+print(len(name)) # prints length of string
+print(name.find("a")) # returns index of first found letter
+print(name.capitalize()) # the first index letter is only capitalized
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha()) # returns false as in name there is whitespace
+print(name.count("i"))
+print(name.replace("i","e"))
+print(name * 3)
