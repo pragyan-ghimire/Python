1e4938633e77c2ccfc01c70feb51cd6501296519
pragyan <ghimirepragyan100@gmail.com>
2025-06-02 07:53:48 +0545

    multiple assignments

---
+++ Day1/multipleAssign.py
@@ -0,0 +1,7 @@
+# multiple variables with different values
+name, age, enthuastic = "Pragyan", 21, True
+print(f"{name} is {age} years old. Enthuastic: {enthuastic}") 
+
+# multiple variables with same values
+skilled = attractive = positive = True
+print(f"skilled: {skilled}, attractive: {attractive}, positive: {positive}")
