4f43afff7da6caa94fe96a7369c7c3f7228e8729
pragyan <ghimirepragyan100@gmail.com>
2025-06-02 12:38:32 +0545

    math functions

---
+++ Day1/mathFunctions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 2
+y = 8
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(4))
+print(max(x,y,z))
+print(min(x,y,z))
