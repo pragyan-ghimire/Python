e6a053f0581547aee0d26df43788dfaa9764b477
pragyan <ghimirepragyan100@gmail.com>
2025-06-03 08:51:01 +0545

    if statements

---
+++ Day1/ifStatements.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you?"))
+
+if age > 30:
+    print("You are eligible to enter this facility")
+elif age >= 18:
+    print("You are adult")
+else:
+    print("You are just a kid hahaha")
