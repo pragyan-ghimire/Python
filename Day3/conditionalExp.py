d6b09f4f92f29d6fe17e4dd89d31f53e634aafe7
pragyan <ghimirepragyan100@gmail.com>
2025-06-04 08:58:49 +0545

    conditional expressions

---
+++ Day3/conditionalExp.py
@@ -0,0 +1,11 @@
+# syntax: X if condition else Y
+
+num = 5
+
+print("positive" if num > 0 else "Negative")
+
+password = "pass"
+print("Access granted" if password == "pass" else "Access Denied")
+
+odd = False if num % 2 == 0 else True
+print(odd)
