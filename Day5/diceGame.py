dade93aa6e0a31b3066de6400baa4c1d06c1b3a4
pragyan <ghimirepragyan100@gmail.com>
2025-06-06 21:44:40 +0545

    dice roller game

---
+++ Day5/diceGame.py
@@ -0,0 +1,90 @@
+import random
+dice_text = ("""
+     _ _          
+    | (_)         
+  __| |_  ___ ___ 
+ / _` | |/ __/ _ \\
+| (_| | | (_|  __/
+ \\__, |_|\\___\\___|
+             
+""")
+
+print(dice_text)
+
+# ● ┌ │ ─ ┐ └ ┘
+
+"┌─────────┐"
+"│         │"
+"│         │"
+"│         │"
+"└─────────┘"
+
+dice_art = {
+    1:(
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"
+    ),
+    2:(
+        "┌─────────┐",
+        "│         │",
+        "│   ● ●   │",
+        "│         │",
+        "└─────────┘"
+    ),
+    3:(
+        "┌─────────┐",
+        "│         │",
+        "│  ● ● ●  │",
+        "│         │",
+        "└─────────┘"
+    ),
+    4:(
+        "┌─────────┐",
+        "│   ● ●   │",
+        "│         │",
+        "│   ● ●   │",
+        "└─────────┘"
+    ),
+    5:(
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"
+    ),
+    6:(
+        "┌─────────┐",
+        "│  ● ● ●  │",
+        "│  ● ● ●  │",
+        "│  ● ● ●  │",
+        "└─────────┘"
+    ),
+}
+
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+
+# print(dice)
+
+# # display line vertically
+# for die in range(num_of_dice):
+#     for line in dice_art.get(dice[die]):
+#         print(line)
+
+# display art horizontally (Tricky baby)
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+for die in dice:
+    total += die
+
+print(f"Total: {total}")
