b2f1c3d5d52955245535f9ace5b24c51a9409ab9
pragyan <ghimirepragyan100@gmail.com>
2025-06-08 17:35:13 +0545

    if __name__ == '__main__':

---
+++ Day7/mainMethod.py
@@ -0,0 +1,9 @@
+# if__name__ == __main__: (this script can be imported OR run standalone)
+#                           Functions and classes in this module can be reused
+#                           without the main block of code executing
+
+def main():
+    print("Not bad")
+
+if __name__ == '__main__':
+    main()
