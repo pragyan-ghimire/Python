d5406b20443b40809fc94322749693c4ecb8023c
pragyan <ghimirepragyan100@gmail.com>
2025-06-08 17:44:06 +0545

    if__name__ == __main__ : use

--- Day7/mainMethod.py
+++ Day7/mainMethod.py
@@ -1,9 +1,19 @@
 # if__name__ == __main__: (this script can be imported OR run standalone)
 #                           Functions and classes in this module can be reused
 #                           without the main block of code executing
+# Good practice (code is modular
+                # helps readability
+                # leaves no global variables
+                # avoid unintended execution)
 
 def main():
     print("Not bad")
+
+def favoriteFood():
+    print("Momos")
+
+print("mainMethod statement")
+favoriteFood()
 
 if __name__ == '__main__':
     main()
