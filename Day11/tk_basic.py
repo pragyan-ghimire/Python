9c817bb2e7890fe603aa4f4b806712eaf6125b19
pragyan <ghimirepragyan100@gmail.com>
2025-06-19 20:46:40 +0545

    tk_basic

---
+++ Day11/tk_basic.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk() # instantiate an instance of a window
+window.geometry("420x420")
+window.title("Basic Gui program")
+# window.config(background= "black")
+window.config(background= "#ff0066")
+
+window.mainloop() # place window on computer screen, listen for events
